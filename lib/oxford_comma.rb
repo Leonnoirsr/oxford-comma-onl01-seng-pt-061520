08c430b3504dc330d6a3cba33b3388d74c22f9c2
Leon Huggins <huggins.leon@gmail.com>
2020-06-27 15:37:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,11 @@ def oxford_comma(array)
  if array.length == 2
    array[-2] << " and "
    array.join
+<<<<<<< HEAD
  elsif array.length == 1
+=======
+ elsif array.length = 1
+>>>>>>> b6eda9f3ee061e68b95beda19716257710598a0b
    array.join
 elsif 2 < array.length
     array[-1].prepend "and "
